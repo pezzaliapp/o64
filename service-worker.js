c14ef2ac202fddf2f57a4545fb73218ef8458464
pezzaliapp <pezzalialessandro@gmail.com>
2025-07-10 16:13:10 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,33 +1,20 @@
-const cacheName = 'pwa-cache-v1';
-const assets = [
-    './',
-    './index.html',
-    './manifest.json',
-    './schedaordine.html',  // Sostituisci con il nome della tua app principale
-    'https://cdnjs.cloudflare.com/ajax/libs/jspdf/2.4.0/jspdf.umd.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/jspdf-autotable/3.5.14/jspdf.plugin.autotable.min.js'
-];
-
-self.addEventListener('install', event => {
-    event.waitUntil(
-        caches.open(cacheName).then(cache => {
-            return cache.addAll(assets);
-        })
-    );
-    self.skipWaiting();
+self.addEventListener('install', function(e) {
+  e.waitUntil(
+    caches.open('o64').then(function(cache) {
+      return cache.addAll([
+        './schedaordine.html',
+        './style.css',
+        './manifest.json',
+        './icon/o64-192.png',
+        './icon/o64-512.png'
+      ]);
+    })
+  );
 });
-
-self.addEventListener('activate', event => {
-    event.waitUntil(
-        caches.keys().then(keys => Promise.all(
-            keys.map(key => key !== cacheName ? caches.delete(key) : null)
-        ))
-    );
-    self.clients.claim();
-});
-
-self.addEventListener('fetch', event => {
-    event.respondWith(
-        caches.match(event.request).then(response => response || fetch(event.request))
-    );
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
 });
